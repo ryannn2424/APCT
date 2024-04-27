0661696e284bfdb64e15e8dc44ce968ea4023549
Ryan Ray <ryanray25@hotmail.com>
2024-04-26 19:40:09 -0700

    jjjjjj

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class MainApp(ctk.CTk):
         self.geometry("400x600")
         self.title("Note Taker")
         self.mode = "Normal"
+        self.isEdited = False
 
         self.columnconfigure(0, weight=1)
 
@@ -24,7 +25,8 @@ class MainApp(ctk.CTk):
 
         self.editingWidget = self.EditingMenu(self)
 
-        self.TopBar(self, self.noteContainer).grid(row=0, column=0, sticky="news")
+        self.topBar = self.TopBar(self, self.noteContainer)
+        self.topBar.grid(row=0, column=0, sticky="news")
 
         self.ButtonBar = self.BottomButtonBar(self)
         self.ButtonBar.grid(row=2, column=0, sticky="news")
@@ -32,14 +34,30 @@ class MainApp(ctk.CTk):
         self.mainloop()
 
     def switchEditMode(self):
+        print("Runnn")
         if self.mode == "Normal":
             if self.noteContainer.selectedCell is not None:
                 self.mode = "Edit"
-                print('run')
+                self.noteContainer.grid_forget()
                 self.editingWidget.grid(row=1, column=0, sticky="news")
                 self.editingWidget.loadMenu(self.noteContainer.selectedCell)
+                self.ButtonBar.actionButton.configure(text="Save Note", command=self.editingWidget.saveToFile)
+                self.ButtonBar.secondaryButton.configure(text="Close Note")
+                self.topBar.NewNoteButton.configure(text="")
+                self.topBar.NewNoteButton.unbind("<Button-1>")
+                self.topBar.deleteNoteButton.configure(text="")
+                self.topBar.deleteNoteButton.unbind("<Button-1>")
         else:
             self.mode = "Normal"
+            self.ButtonBar.secondaryButton.configure(text="Settings")
+            self.editingWidget.grid_forget()
+            self.ButtonBar.actionButton.configure(text="Edit Selected Note", command=self.ButtonBar.actionButtonCommand)
+            self.noteContainer.grid(row=1, column=0, sticky="news")
+            self.topBar.NewNoteButton.configure(text="")
+            self.topBar.NewNoteButton.bind("<Button-1>", lambda event: self.topBar.createNewNoteGUI())
+            self.topBar.deleteNoteButton.configure(text="")
+            self.topBar.deleteNoteButton.bind("<Button-1>", lambda event: self.topBar.DeleteMode())
+
 
     class EditingMenu(ctk.CTkFrame):
         def __init__(self, master, selectedCell=None, **kwargs):
@@ -47,22 +65,94 @@ class MainApp(ctk.CTk):
             
             self.columnconfigure(0, weight=1)
             self.rowconfigure(0, weight=1)
+            self.rowconfigure(1, weight=20)
 
             if selectedCell is not None:
                 self.selectedCell = selectedCell
                 self.icon = selectedCell.image
 
-            self.iconLabel = ctk.CTkLabel(self, text="")
-            self.iconLabel.grid(row=0, column=0, sticky="news")
+            infoFrame = ctk.CTkFrame(self)
+            infoFrame.grid(row=0, column=0, sticky="news")
+            infoFrame.rowconfigure(0, weight=1)
+
+            self.iconLabel = ctk.CTkLabel(infoFrame, text="")
+            self.iconLabel.grid(row=0, column=0, sticky="ns", padx=5)
+            self.iconLabel.bind("<Button-1>", lambda event : self.changeIcon())
+
+            self.noteTitle = ctk.CTkLabel(infoFrame, text="NoteTitle", font=ctk.CTkFont("Ariel", size=24))
+            self.noteTitle.grid(row=0, column=1, sticky="ns", padx=5)
+            self.noteTitle.bind("<Button-1>", lambda event : self.renameNote())
+
+            self.noteText = ctk.CTkTextbox(self)
+            self.noteText.grid(row=1, column=0, sticky="news")
+            self.noteText.bind("<KeyPress>", lambda event: self.changeEditStatus())
+
+        def changeEditStatus(self):
+            self.master.isEdited = True
+
+        def renameNote(self):
+            newName = ctk.CTkInputDialog(text="Enter new note name", title="Rename Note").get_input()
+            if newName is not None:
+                self.noteTitle.configure(text=newName)
+                self.selectedCell.name = newName
+                self.selectedCell.nameLabel.configure(text=newName)
+
+                with open(self.selectedCell.filelocation, "r") as file:
+                    lines = file.readlines()
+
+                lines[0] = f"{newName}\n"
+
+                with open(self.selectedCell.filelocation, "w") as file:
+                    file.writelines(lines)
+
+                directory, filename = os.path.split(self.selectedCell.filelocation)
+                os.rename(self.selectedCell.filelocation, os.path.join(directory, newName + ".txt"))
+                self.selectedCell.filelocation = os.path.join(directory, newName + ".txt")
+
+        def changeIcon(self):
+            newIconPath = filedialog.askopenfile().name
+            # print(newIconPath)
+
+            with open(self.selectedCell.filelocation, "r") as file:
+                    lines = file.readlines()
+
+            lines[2] = f"{newIconPath}\n"
+
+            with open(self.selectedCell.filelocation, "w") as file:
+                file.writelines(lines)
+
+            newImage = ctk.CTkImage(Image.open(newIconPath), size=(50, 50))
+            self.iconLabel.configure(image=newImage)
+            self.selectedCell.iconLabel.configure(image=newImage)
+            
 
         def loadMenu(self, selectedCell):
             self.selectedCell = selectedCell
             self.icon = selectedCell.image
-
             self.iconLabel.configure(image=self.icon)
+            self.noteTitle.configure(text=self.selectedCell.name)
 
+            self.noteText.delete("0.0", "end")
 
+            with open(self.selectedCell.filelocation, "r") as file:
+                self.noteText.insert("0.0", ''.join(file.readlines()[3:]))
 
+        def saveToFile(self, filepath=None):
+            if filepath is None:
+                filepath = self.selectedCell.filelocation
+            with open(filepath, "r") as file:
+                noteMetadata = []
+                noteMetadata.append(file.readline().strip())
+                noteMetadata.append(file.readline().strip())
+                noteMetadata.append(file.readline().strip())
+
+            with open(filepath, 'w') as file:
+                newNote = f"{noteMetadata[0]}\n{noteMetadata[1]}\n{noteMetadata[2]}\n{self.noteText.get("0.0", "end")}"
+                file.write(newNote)
+
+            self.master.isEdited = False
+
+        
 
     class BottomButtonBar(ctk.CTkFrame):
         def __init__(self, master, **kwargs):
@@ -75,14 +165,26 @@ class MainApp(ctk.CTk):
             self.actionButton = ctk.CTkButton(self, text="Edit Selected Note", command=lambda: self.actionButtonCommand())
             self.actionButton.grid(column=0, row=0, sticky="news", padx=2)
 
-            ctk.CTkButton(self, text="Settings").grid(column=1, row=0, sticky="news", padx=2)
+            self.secondaryButton = ctk.CTkButton(self, text="Settings", command=lambda: self.secondaryButtonCommand())
+            self.secondaryButton.grid(column=1, row=0, sticky="news", padx=2)
 
         def actionButtonCommand(self):
+            print(self.master.mode)
             if self.master.mode == "Delete":
                 if tmsg.askyesno("Delete Note", "Are you sure you want to delete this note?"):
                     self.master.noteContainer.selectedCell.deleteSelf()
             elif self.master.mode == "Normal":
                 self.master.switchEditMode()
+
+        def secondaryButtonCommand(self):
+            if self.master.mode == "Edit":
+                # print("ran")
+                if self.master.isEdited:
+                    if tmsg.askyesno("Unsaved File", "You have unsaved changes. Are you sure you want to close this note?"):
+                        self.master.switchEditMode()
+                else:
+                    self.master.switchEditMode()
+
 
 
     class NoteContainer(ctk.CTkFrame):
@@ -110,13 +212,14 @@ class MainApp(ctk.CTk):
                     filePath = f"Notes/{file}"
                     self.addCell(name, iconPath, date, filePath)
 
-        def addCell(self, name, iconPath, date, filepath=None):
+        def addCell(self, name, iconPath, date, filepath=None, createFile=False):
             if filepath == None: filepath = f"Notes/{name}.txt"
             newCell = self.cellClassRef(self, name=name, icon=iconPath, date=date, filelocation=filepath)
             newCell.grid(row=self.noteIndex, column=0, sticky="news", pady=2)
             # self.cellList.append(newCell)
-            with open(f"Notes/{name}.txt", "w") as noteFile:
-                noteFile.write(f"{name}\n{date}\n{iconPath}\n")
+            if createFile:
+                with open(f"Notes/{name}.txt", "w") as noteFile:
+                    noteFile.write(f"{name}\n{date}\n{iconPath}\n")
             self.noteIndex += 1
 
     class TopBar(ctk.CTkFrame):
@@ -132,9 +235,9 @@ class MainApp(ctk.CTk):
 
             self.currentDate = datetime.datetime.now()
 
-            NewNoteButton = ctk.CTkLabel(self, text="", font=ctk.CTkFont(family="JetBrainsMono NFM Regular", size=40))
-            NewNoteButton.grid(row=0, column=0, sticky="news", padx=5)
-            NewNoteButton.bind("<Button-1>", lambda event: self.createNewNoteGUI())
+            self.NewNoteButton = ctk.CTkLabel(self, text="", font=ctk.CTkFont(family="JetBrainsMono NFM Regular", size=40))
+            self.NewNoteButton.grid(row=0, column=0, sticky="news", padx=5)
+            self.NewNoteButton.bind("<Button-1>", lambda event: self.createNewNoteGUI())
 
             nameAndDateFrame = ctk.CTkFrame(self, fg_color="transparent")
             nameAndDateFrame.grid(column=1, row=0, sticky="news")
@@ -238,7 +341,7 @@ class MainApp(ctk.CTk):
             def createCell():
                 if self.selectedImage == "./openFile.png":
                     self.selectedImage = "./image-not-found-icon.png"
-                self.cellContainerRef.addCell(Entry.get(), self.selectedImage, f"{self.currentDate.strftime("%B")} {self.currentDate.strftime("%d")}, {self.currentDate.strftime("%Y")} - {self.currentDate.strftime('%I')}: {self.currentDate.strftime('%M')} {self.currentDate.strftime('%p')}")
+                self.cellContainerRef.addCell(Entry.get(), self.selectedImage, f"{self.currentDate.strftime("%B")} {self.currentDate.strftime("%d")}, {self.currentDate.strftime("%Y")} - {self.currentDate.strftime('%I')}: {self.currentDate.strftime('%M')} {self.currentDate.strftime('%p')}", createFile=True)
                 popup.destroy()
 
     class NoteCell(ctk.CTkFrame):
@@ -249,6 +352,7 @@ class MainApp(ctk.CTk):
             self.columnconfigure(1, weight=100)
 
             self.filelocation = filelocation
+            self.name = name
             backgroundColor = ("#FFFFFF", "#3A3B3C")
 
             self.image = ctk.CTkImage(light_image=Image.open(icon), dark_image=Image.open(icon), size=(50, 50))
@@ -258,25 +362,25 @@ class MainApp(ctk.CTk):
             self.IconFrame.rowconfigure(0, weight=1)
             self.IconFrame.columnconfigure(0, weight=1)
 
-            iconLabel = ctk.CTkLabel(self, text="", image=self.image, bg_color=backgroundColor)
-            iconLabel.grid(column=0, row=0, sticky="news")
+            self.iconLabel = ctk.CTkLabel(self, text="", image=self.image, bg_color=backgroundColor)
+            self.iconLabel.grid(column=0, row=0, sticky="news")
 
             self.NameFrame = ctk.CTkFrame(self, height=70, fg_color=backgroundColor)
             self.NameFrame.grid(column=1, row=0, sticky="news")
             self.NameFrame.rowconfigure(0, weight=14)
             self.NameFrame.rowconfigure(1, weight=1)
 
-            nameLabel = ctk.CTkLabel(self.NameFrame, text=name, font=ctk.CTkFont("Ariel", size=24), anchor="sw")
-            nameLabel.grid(column=0, row=0, sticky="news")
+            self.nameLabel = ctk.CTkLabel(self.NameFrame, text=name, font=ctk.CTkFont("Ariel", size=24), anchor="sw")
+            self.nameLabel.grid(column=0, row=0, sticky="news")
             
             dateLabel = ctk.CTkLabel(self.NameFrame, text=date, font=ctk.CTkFont("Ariel", size=12), anchor="w")
             dateLabel.grid(column=0, row=1, sticky="news")
 
             self.bind("<Button-1>", lambda event: self.selectSelf())
             self.IconFrame.bind("<Button-1>", lambda event: selectSelf())
-            iconLabel.bind("<Button-1>", lambda event: selectSelf())
+            self.iconLabel.bind("<Button-1>", lambda event: selectSelf())
             self.NameFrame.bind("<Button-1>", lambda event: selectSelf())
-            nameLabel.bind("<Button-1>", lambda event: selectSelf())
+            self.nameLabel.bind("<Button-1>", lambda event: selectSelf())
             dateLabel.bind("<Button-1>", lambda event: selectSelf())
 
 
